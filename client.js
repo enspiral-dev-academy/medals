09e84a304d666ab0a91804c62710d51b58fba159
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-05-14 15:05:34 +1200

    update to ws

--- client.js
+++ client.js
@@ -115,7 +115,7 @@ mode(function (m) {
 })
 
 require('./reconnect')(function (cb) {
-  var ws = WS.connect('wss://quiet-brook-61744.herokuapp.com:8000')
+  var ws = WS.connect('ws://quiet-brook-61744.herokuapp.com:8000/')
   //var ws = WS.connect('ws://localhost:8000/')
 
   client = window.CLIENT = MuxRpc(require('./manifest.json'), null, JSONDL)()
