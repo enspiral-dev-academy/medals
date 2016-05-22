9b0c8f3347fa8a5f54d3e0dda4dcb845e933d18b
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-05-22 14:33:33 +1200

    testing

--- client.js
+++ client.js
@@ -145,7 +145,7 @@ mode(function (m) {
 })
 
 require('./reconnect')(function (cb) {
-  var ws = WS.connect('ws://http://124.150.139.107:8000/')
+  var ws = WS.connect('ws://124.150.139.107:8000/')
   //var ws = WS.connect('ws://localhost:8000/')
 
   client = window.CLIENT = MuxRpc(require('./manifest.json'), null, JSONDL)()
