f2667b27874eafbb4a1908bb2397e9e28123f04a
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-05-22 19:31:59 +1200

    add starting db

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var Auth = require('ticket-auth')
 var Tiny = require('tiny-route')
 
 var config = require('rc')('eda-grads', {
-  path: path.join(process.env.HOME, 'storage/.eda-grads'),
+  path: path.join('storage', '.eda-grads'),
   port: 8000,
 })
 
