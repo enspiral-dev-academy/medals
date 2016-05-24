28b2332548264440132852e30f4e858f3fdf4e46
Dan Lewis <dan.lewis@devacademy.co.nz>
2016-05-24 18:51:14 +1200

    use if for local vs prod

--- client.js
+++ client.js
@@ -145,8 +145,14 @@ mode(function (m) {
 })
 
 require('./reconnect')(function (cb) {
-  var ws = WS.connect('ws://124.150.139.107/')
-  //var ws = WS.connect('ws://localhost:8000/')
+  if (process.env.NODE_ENV == 'production') {
+      var ws = WS.connect('ws://124.150.139.107/')
+  } 
+  else {
+      var ws = WS.connect('ws://localhost:8000/')
+  }
+
+
 
   client = window.CLIENT = MuxRpc(require('./manifest.json'), null, JSONDL)()
 
