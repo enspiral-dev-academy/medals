59fe0d490e22b225393799d6a4d9a647822031e3
Tian Dutoit <tiandutoit.nz@gmail.com>
2018-02-22 15:49:11 +1300

    assessment lists the topics

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const express = require('express')
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/users')
+const topicRoutes = require('./routes/topics')
 
 const server = express()
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/auth', authRoutes)
 server.use('/api/v1/users', userRoutes)
+server.use('/api/v1/topic', topicRoutes)
 
 // Default route for non-API requests
 server.get('*', (req, res) => {
