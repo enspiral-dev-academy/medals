9916a99ce9f1f0baa758e2ef37a247d3485cd440
Tian Dutoit <tiandutoit.nz@gmail.com>
2018-02-23 13:17:31 +1300

    assessment componenet accessing tags db

--- server/routes/topics.js
+++ server/routes/topics.js
@@ -1,14 +1,31 @@
+// import {getTopics} from '../../client/actions/assessments'
+
 const express = require('express')
 const bodyParser = require('body-parser')
 
-const topics = require('../../client/components/assessments/topics.json') // will be a db eventually
+// const topics = require('../../client/components/assessments/topics.json') // will be a db eventually
 const router = express.Router()
+const development = require('../db/knexfile').development
+const knex = require('knex')(development)
 
 router.use(bodyParser.json())
 
+// router.get('/', (req, res) => {
+//   res.json(topics)
+//   // do we need a catch?
+// })
+
 router.get('/', (req, res) => {
-  res.json(topics)
+  getList()
+    .then(function (topics) {
+      console.log(topics)
+      res.send({topics})
+    })
   // do we need a catch?
 })
+
+const getList = () => {
+  return knex('quiz_tags').select('tag')
+}
 
 module.exports = router
