1546abdaa92373142f1b29ef563c036e2efed513
Kristina Muller <kristina.oneness@gmail.com>
2018-02-23 15:19:52 +1300

    edited seed data

--- test/server/db/seeds/4-tasks.js
+++ test/server/db/seeds/4-tasks.js
@@ -1,8 +1,14 @@
 exports.seed = (knex, Promise) => {
   return knex('tasks').insert([
-    {id: 1, description: 'hello', assignment_id: 1},
-    {id: 2, description: 'goodbye', assignment_id: 2},
-    {id: 3, description: 'smiles', assignment_id: 3},
-    {id: 4, description: 'rainbows', assignment_id: 4}
+    {id: 1, description: 'Make a github account', assignment_id: 1},
+    {id: 2, description: 'Javascript Basics on Free Code Camp', assignment_id: 2},
+    {id: 3, description: 'Start Toggle', assignment_id: 3},
+    {id: 4, description: 'Cultural Blog', assignment_id: 4},
+    {id: 5, description: 'Feedback', assignment_id: 2},
+    {id: 6, description: 'Start Toggle', assignment_id: 3},
+    {id: 7, description: 'Technical Blog', assignment_id: 4},
+    {id: 8, description: 'Javascript Basics on Free Code Camp', assignment_id: 2},
+    {id: 9, description: 'Download Skeleton Framework', assignment_id: 3},
+    {id: 10, description: 'Export Toggle Timesheet', assignment_id: 4}
   ])
 }
