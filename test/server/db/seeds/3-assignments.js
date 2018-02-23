d429f74791e03ebf369ab0a9f482c08f09a55b88
Steve Lui <steve.liu.3029@gmail.com>
2018-02-23 15:46:30 +1300

    added task component

--- test/server/db/seeds/3-assignments.js
+++ test/server/db/seeds/3-assignments.js
@@ -1,14 +1,15 @@
 exports.seed = (knex, Promise) => {
   return knex('assignments').insert([
-    {id: 1, title: 'Assignment 1', sprint_id: 1},
-    {id: 2, title: 'Assignment 2: Minesweeper', sprint_id: 1},
-    {id: 3, title: 'Assignment 3', sprint_id: 1},
-    {id: 4, title: 'Assignment 4', sprint_id: 1},
-    {id: 5, title: 'Assignment 5', sprint_id: 1},
-    {id: 6, title: 'Assignment 6', sprint_id: 1},
-    {id: 7, title: 'Assignment 7', sprint_id: 1},
-    {id: 8, title: 'Assignment 8', sprint_id: 1},
-    {id: 9, title: 'Assignment 9', sprint_id: 1},
-    {id: 10, title: 'Assignment 10', sprint_id: 1}
+    {id: 1, title: 'How to waffle', sprint_id: 1},
+    {id: 2, title: 'Track time', sprint_id: 1},
+    {id: 3, title: 'Command line and Github', sprint_id: 1},
+    {id: 4, title: 'Create website blog', sprint_id: 1},
+    {id: 5, title: 'Introduce yourself', sprint_id: 1},
+    {id: 6, title: 'Udacity HTML CSS course 1', sprint_id: 2},
+    {id: 7, title: 'Udacity Practice', sprint_id: 2},
+    {id: 8, title: 'Udacity blog 1', sprint_id: 2},
+    {id: 9, title: 'Udacity HTML CSS course 2', sprint_id: 2},
+    {id: 10, title: 'Udacity blog 2', sprint_id: 2},
+    {id: 11, title: 'Technical debt', sprint_id: 2}
   ])
 }
