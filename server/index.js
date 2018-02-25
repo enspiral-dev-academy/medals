dbc451bca665d25d8ca5927b50ab5765c65100c1
Emily Parkes <emilycoco@me.com>
2018-02-26 10:00:53 +1300

    modify suggested changes by don

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
-var server = require('./server')
+const server = require('./server')
 
-var PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
-
   // eslint-disable-next-line no-console
-  // console.log('Listening on port', PORT)
+  console.log('Listening on port', PORT)
 })
