070e9fc42410080ca4b9820c014af08f3c5ab793
adam-kuhn <adam.kuhn20@gmail.com>
2018-02-26 10:18:13 +1300

    convert Check back to functional comp

--- client/components/assessments/Check.jsx
+++ client/components/assessments/Check.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
 
-class Check extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-    }
-    // this.state.handleClick = this.handleClick.bind(this)
-  }
-
-  render () {
-    return (
-      <div>
-        <h3>{this.props.feedback.check}</h3>
-        <p>{this.props.feedback.reason}</p>
-        <button onClick={this.props.displayNext} type='button'>Next Question</button>
-      </div>
-    )
-  }
+const Check = (props) => {
+  return (
+    <div>
+      <h3>{props.feedback.check}</h3>
+      <p>{props.feedback.reason}</p>
+      <button onClick={props.displayNext} type='button'>Next Question</button>
+    </div>
+  )
 }
 
 export default Check
