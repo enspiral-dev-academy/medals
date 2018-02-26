641e4f44d56c9a95e17d2b569019a49c1c24f5a5
Tian Dutoit <tiandutoit.nz@gmail.com>
2018-02-26 15:18:44 +1300

    fixed some code reviews

--- client/components/assessments/AssessmentHome.jsx
+++ client/components/assessments/AssessmentHome.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import {getQuestions} from '../../actions/assessments'
 import {connect} from 'react-redux'
 
