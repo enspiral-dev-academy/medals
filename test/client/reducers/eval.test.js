e5b15eacdbc5c22e03722d39b7501642c353131d
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-27 11:46:27 +1300

    evalQuestions reducer test passed

--- test/client/reducers/eval.test.js
+++ test/client/reducers/eval.test.js
@@ -1,5 +1,6 @@
-import {GET_TAGS} from '../../../client/actions/eval'
+import {GET_TAGS, FETCH_QUESTIONS} from '../../../client/actions/eval'
 import evalTags from '../../../client/reducers/eval-tags'
+import evalQuestions from '../../../client/reducers/eval-questions'
 
 const state = ''
 
@@ -8,5 +9,14 @@ test('evalTags reducer updates state with tags', () => {
     type: GET_TAGS,
     tags: ['tag1', 'tag2']}
   const newState = evalTags(state, action)
+  expect(newState.length).toBe(2)
+})
+
+test('evalQuestions reducer updates state with question', () => {
+  const action = {
+    type: FETCH_QUESTIONS,
+    questions: ['question1', 'question2']
+  }
+  const newState = evalQuestions(state, action)
   expect(newState.length).toBe(2)
 })
