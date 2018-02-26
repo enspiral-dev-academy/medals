2623a8195132fbdb25cfacb023ee41533067b099
adam-kuhn <adam.kuhn20@gmail.com>
2018-02-27 10:21:15 +1300

    using questions from databse, skip fail old test

--- client/components/eval/EvalMe.jsx
+++ client/components/eval/EvalMe.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
 import Check from './Check'
 
 import sampleData from './questions.json'
 
 // mock data
-const sampleQues = sampleData.questions
+// const sampleQues = sampleData.questions
 
 class EvalMe extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      question: sampleQues,
+      // question: sampleQues,
       questionIndx: 0,
       answer: '',
       key: '',
@@ -50,7 +52,7 @@ class EvalMe extends React.Component {
     }
   }
   displayNext () {
-    if (sampleQues.length === this.state.questionIndx + 1) {
+    if (this.props.evalQuestions.length === this.state.questionIndx + 1) {
       this.props.history.push('complete')
     }
     this.setState({
@@ -61,21 +63,23 @@ class EvalMe extends React.Component {
   }
 
   render () {
+    console.log(this.props.evalQuestions)
+    console.log(this.props)
     return (
       <div className='eval-me'>
-        <h2>{this.state.question[this.state.questionIndx].question}</h2>
+        <h2>{this.props.evalQuestions[this.state.questionIndx].question}</h2>
         <form>
 
-          {this.state.question[this.state.questionIndx].choices.map((answer, idx) => {
+          {this.props.evalQuestions[this.state.questionIndx].responses.map((answer) => {
             return (
-              <div key={idx}>
+              <div key={answer.id}>
                 <input readOnly type='radio' name='answer'
-                  value={answer.key} data-ans={answer.ans}
-                  data-fbk={answer.fbk}
+                  value={answer.key} data-ans={answer.response}
+                  data-fbk={answer.reason}
                   onChange={this.handleChange}
-                  checked={this.state.answer === answer.ans}
+                  checked={this.state.answer === answer.response}
                   disabled={(this.state.disabled) ? 'disabled' : ''} />
-                <label>&nbsp;{answer.ans}</label>
+                <label>&nbsp;{answer.response}</label>
               </div>)
           })}
         </form>
@@ -86,4 +90,10 @@ class EvalMe extends React.Component {
   }
 }
 
-export default EvalMe
+function mapStateToProps (state) {
+  return {
+    evalQuestions: state.evalQuestions
+  }
+}
+
+export default connect(mapStateToProps)(EvalMe)
