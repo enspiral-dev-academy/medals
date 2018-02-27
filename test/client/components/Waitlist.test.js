b07af71dc31ac6e8102e34e249ccc1d0da731494
Don Smith <don@antiantonym.com>
2018-02-27 13:45:44 +1300

    make waitlist test pass

--- test/client/components/Waitlist.test.js
+++ test/client/components/Waitlist.test.js
@@ -1,29 +1,25 @@
 import React from 'react'
 import {mount} from 'enzyme'
-import {Provider} from 'react-redux'
-import configureStore from 'redux-mock-store'
-import {MemoryRouter as Router} from 'react-router'
 
-import WaitList from '../../../client/components/WaitList'
+import {WaitList} from '../../../client/components/WaitList'
 
-test.skip('<WaitList /> Displays User Table When route is accessed', () => {
-  const mockStore = configureStore()({
-    allUsers: [{
-      ghid: 3232,
-      id: 12,
-      username: test
-    }]
-  })
+WaitList.prototype.componentDidMount = () => {}
 
-  WaitList.prototype.componentDidMount = () => {}
+test('<WaitList /> Displays User Table When route is accessed', () => {
+  const allUsers = [{
+    id: 1,
+    ghid: 9,
+    username: 'test user 1'
+  }, {
+    id: 2,
+    ghid: 8,
+    username: 'test user 2'
+  }, {
+    id: 3,
+    ghid: 7,
+    username: 'test user 3'
+  }]
+  const wrapper = mount(<WaitList allUsers={allUsers} />)
 
-  const wrapper = mount(
-    <Provider store={mockStore}>
-      <Router>
-        <WaitList />
-      </Router>
-    </Provider>
-  )
-
-  expect(wrapper.find('th')).to.have.length(3)
+  expect(wrapper.find('th').length).toBe(3)
 })
