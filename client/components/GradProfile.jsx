3d96f82c4705be838dce17097a70929a88da340f
Emily Parkes <emilycoco@me.com>
2018-02-27 15:00:51 +1300

    edit import

--- client/components/GradProfile.jsx
+++ client/components/GradProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-import {profile} from './Profile'
+import {Profile} from './Profile'
 import {getGradProfile} from '../actions/gradProfile'
 
 class GradProfile extends React.Component {
