bb957690594e82a1e0f03f817f5c882d69451551
Judy Ting-Edwards <judy.yun.ting@gmail.com>
2018-02-27 16:13:23 +1300

    serverside complete!!!

--- server/db/user-profiles.js
+++ server/db/user-profiles.js
@@ -2,13 +2,31 @@ const connection = require('./connection')
 // const hash = require('../auth/hash')
 
 module.exports = {
-  getUserByName
+  getProfileByUserId,
+  editProfile
 }
 
-function getUserByName (id, conn) {
+function getProfileByUserId (id, conn) {
   const db = conn || connection
-  return db('user-profiles')
-    .join('users', 'user-profiles.username', 'users.username')
-    .where('users.username')
-    .select('user-profiles.id', 'user-profiles.firstName', 'user-profiles.')
+  return db('user_profiles')
+    .join('users', 'user_profiles.user_id', 'users.id')
+    .where('users.id', id)
+    .select()
+    .first()
+}
+
+function editProfile (profileData, userId, conn) {
+  const db = conn || connection
+  const {firstName, preferredName, surname, profilePic, email, phone, bio} = profileData
+  return db('user_profiles')
+    .where('user_id', userId)
+    .update({
+      firstName: firstName,
+      preferredName: preferredName,
+      surname: surname,
+      profilePic: profilePic,
+      email: email,
+      phone: phone,
+      bio: bio
+    })
 }
